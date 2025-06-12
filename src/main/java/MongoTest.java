1c78909f6987a58afd340c974c53a9a73fbd8d8a
shahrukh hussain <shahrukh.hussain@digital.homeoffice.gov.uk>
2025-06-12 14:14:17 +0100

    can do all CRUD actions

--- src/main/java/MongoTest.java
+++ src/main/java/MongoTest.java
@@ -2,6 +2,7 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.result.DeleteResult;
 import org.bson.Document;
 
 public class MongoTest {
@@ -13,14 +14,30 @@ public class MongoTest {
         MongoDatabase database = mongoClient.getDatabase("cpafc-test");
 
         MongoCollection<Document> collection = database.getCollection("children");
+         collection.drop();
 
         Document child = new Document("firstName", "Rio")
                 .append("lastName", "Lacey")
                 .append("age", 12)
-                .append("team", "Under 13 Lions");
+                .append("team", "U13 Lions");
 
         collection.insertOne(child);
-         System.out.println("Inserted child" + child.toJson());
+        System.out.println("Inserted child" + child.toJson());
+
+        // Find a child by first name + last name
+        Document query = new Document("firstName", "Rio").append("lastName", "Lacey");
+        // Update the team
+        Document update = new Document("$set", new Document("team", "U13 Tigers"));
+
+        collection.updateOne(query, update);
+
+        System.out.println("✅ Updated Rio's team to U13 Tigers");
+
+         // DELETE child by first name and last name
+         Document deleteQuery = new Document("firstName", "Rio").append("lastName", "Lacey");
+         DeleteResult deleteResult = collection.deleteOne(deleteQuery);
+
+         System.out.println("🗑️ Deleted count: " + deleteResult.getDeletedCount());
 
          for (Document doc : collection.find()) {
              System.out.println("👦 Child found: " + doc.toJson());

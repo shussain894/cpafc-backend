f4b059e4017e6bca5b9a9764b866d8630a5e6af1
shahrukh hussain <shahrukh.hussain@digital.homeoffice.gov.uk>
2025-06-18 13:53:00 +0100

    added childData and insertMany

---
+++ src/main/java/ChildData.java
@@ -0,0 +1,36 @@
+import org.bson.Document;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChildData {
+    public static List<Document> getSampleChildren() {
+        List<Document> children = new ArrayList<>();
+
+        children.add(new Document("firstName", "Rio")
+                .append("lastName", "Lacey")
+                .append("age", 12)
+                .append("team", "U13 Tigers"));
+
+        children.add(new Document("firstName", "Amir")
+                .append("lastName", "Khan")
+                .append("age", 12)
+                .append("team", "U13 Tigers"));
+
+        children.add(new Document("firstName", "Kaylen")
+                .append("lastName", "Smith")
+                .append("age", 12)
+                .append("team", "U13 Tigers"));
+
+        children.add(new Document("firstName", "Lamar")
+                .append("lastName", "Jackson")
+                .append("age", 12)
+                .append("team", "U13 Tigers"));
+
+        children.add(new Document("firstName", "Lena")
+                .append("lastName", "Smith")
+                .append("age", 10)
+                .append("team", "Under 11 Lions"));
+
+        return children;
+    }
+}
--- src/main/java/MongoTest.java
+++ src/main/java/MongoTest.java
@@ -1,9 +1,8 @@
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoCollection;
+import com.mongodb.client.*;
 import com.mongodb.client.result.DeleteResult;
 import org.bson.Document;
+import java.util.List;
+
 
 public class MongoTest {
     public static void main(String[] args) {
@@ -16,13 +15,9 @@ public class MongoTest {
         MongoCollection<Document> collection = database.getCollection("children");
          collection.drop();
 
-        Document child = new Document("firstName", "Rio")
-                .append("lastName", "Lacey")
-                .append("age", 12)
-                .append("team", "U13 Lions");
-
-        collection.insertOne(child);
-        System.out.println("Inserted child" + child.toJson());
+         List<Document> children = ChildData.getSampleChildren();
+         collection.insertMany(children);
+         System.out.println("Sample children inserted!");
 
         // Find a child by first name + last name
         Document query = new Document("firstName", "Rio").append("lastName", "Lacey");
@@ -31,16 +26,25 @@ public class MongoTest {
 
         collection.updateOne(query, update);
 
-        System.out.println("✅ Updated Rio's team to U13 Tigers");
+        System.out.println("Updated Rio's team to U13 Tigers");
 
          // DELETE child by first name and last name
          Document deleteQuery = new Document("firstName", "Rio").append("lastName", "Lacey");
          DeleteResult deleteResult = collection.deleteOne(deleteQuery);
 
-         System.out.println("🗑️ Deleted count: " + deleteResult.getDeletedCount());
+         System.out.println("Deleted count: " + deleteResult.getDeletedCount());
 
          for (Document doc : collection.find()) {
-             System.out.println("👦 Child found: " + doc.toJson());
+             System.out.println("Child found: " + doc.toJson());
+         }
+
+         // FIND children by team
+         String teamToFind = "U13 Tigers";
+         FindIterable<Document> teamChildren = collection.find(new Document("team", teamToFind));
+
+         System.out.println("Children playing for " + teamToFind + ":");
+         for (Document doc : teamChildren) {
+             System.out.println("Child found: " + doc.toJson());
          }
 
      }

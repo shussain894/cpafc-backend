7c394a176f267786ccc7e368848937d305071b90
shahrukh hussain <shahrukh.hussain@digital.homeoffice.gov.uk>
2025-06-19 13:00:51 +0100

    added method to insertChildren from data, added update and delete child method

--- src/main/java/children/ChildService.java
+++ src/main/java/children/ChildService.java
@@ -4,6 +4,9 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.UpdateResult;
+import com.mongodb.client.result.DeleteResult;
+import children.ChildData;
 
 import org.bson.Document;
 
@@ -19,14 +22,13 @@ public class ChildService {
         childrenCollection = database.getCollection("children");
     }
 
-    public void insertChild(Child child) {
-        Document doc = new Document("firstName", child.getFirstName())
-                .append("lastName", child.getLastName())
-                .append("age", child.getAge())
-                .append("team", child.getTeam());
-        childrenCollection.insertOne(doc);
-        System.out.println("Inserted: " + child);
+    public void insertChildren(List<Document> children) {
+        for (Document doc : children) {
+            childrenCollection.insertOne(doc);
+            System.out.println("child added " + doc.getString("firstName") + " " + doc.getString("lastName"));
+        }
     }
+
 
     public List<Child> getAllChildren() {
         List<Child> children = new ArrayList<>();
@@ -43,13 +45,58 @@ public class ChildService {
     public static void main(String[] args) {
         ChildService service = new ChildService();
 
-        Child rio = new Child("Rio", "Lacey", 12, "U13 Lions");
-        service.insertChild(rio);
+        service.childrenCollection.drop();
 
-        List<Child> allChildren = service.getAllChildren();
-        for (Child child : allChildren) {
-            System.out.println("child added " + child);
+        service.insertChildren(ChildData.getSampleChildren());
+
+        boolean updated = service.updateChildTeam("Rio", "Lacey", "U13 Tigers");
+        System.out.println("Update success: " + updated);
+
+        boolean assigned = service.assignChildToTeam("Kaylen", "Smith", "Under 13 Tigers");
+        System.out.println("Assign success: " + assigned);
+
+        boolean deleted = service.deleteChild("Amir", "Khan");
+        System.out.println("Delete success: " + deleted);
+
+        System.out.println("All children in DB:");
+        service.getAllChildren().forEach(System.out::println);
+    }
+
+
+    public boolean updateChildTeam(String firstName, String lastName, String newTeam) {
+        Document filter = new Document("firstName", firstName)
+                .append("lastName", lastName);
+
+        Document update = new Document("$set", new Document("team", newTeam));
+
+        UpdateResult result = childrenCollection.updateOne(filter, update);
+
+        if (result.getModifiedCount() > 0) {
+            System.out.println("Updated " + firstName + "'s team to " + newTeam);
+            return true;
+        } else {
+            System.out.println("No matching child found to update.");
+            return false;
         }
+    }
+
+    public boolean deleteChild(String firstName, String lastName) {
+        Document filter = new Document("firstName", firstName)
+                .append("lastName", lastName);
+
+        DeleteResult result = childrenCollection.deleteOne(filter);
+
+        if (result.getDeletedCount() > 0) {
+            System.out.println("Deleted child: " + firstName + " " + lastName);
+            return true;
+        } else {
+            System.out.println("No matching child found to delete.");
+            return false;
+        }
+    }
+
+    public boolean assignChildToTeam(String firstName, String lastName, String team) {
+        return updateChildTeam(firstName, lastName, team);
     }
 
 }

186eea0b861672f3865c83e628816e85d2009583
shahrukh hussain <shahrukh.hussain@digital.homeoffice.gov.uk>
2025-07-21 17:40:08 +0100

    fixed isAdmin issue for login

--- src/main/java/com/cpafc/controller/LoginController.java
+++ src/main/java/com/cpafc/controller/LoginController.java
@@ -1,26 +1,29 @@
 package com.cpafc.controller;
 
-import com.cpafc.model.Admin;
-import com.cpafc.repository.AdminRepository;
+import com.cpafc.model.Coach;
+import com.cpafc.model.Role;
+import com.cpafc.repository.CoachRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
 public class LoginController {
 
     @Autowired
-    private AdminRepository adminRepository;
+    private CoachRepository coachRepository;
 
     @PostMapping("/login")
     public ResponseEntity<Map<String, Object>> login(@RequestBody Map<String, String> request) {
         String email = request.get("email");
 
-        boolean isAdmin = adminRepository.findByEmail(email).isPresent();
+        Optional<Coach> optionalCoach = coachRepository.findByEmail(email);
+        boolean isAdmin = optionalCoach.isPresent() && optionalCoach.get().getRole() == Role.ADMIN;
 
         Map<String, Object> response = new HashMap<>();
         response.put("email", email);
